3df740ab8820b7957a254c2cb4ab771f0c940e40
Pouria Jahandideh <pjsofts@gmail.com>
2025-05-08 15:02:19 +0330

    added .env

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000"
+const url = import.meta.env.VITE_API_URL
 async function getProduct(){
     const repsonse = await fetch(url + "/products")
     const data = await repsonse.json()
